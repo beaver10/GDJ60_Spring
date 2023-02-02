4e52ae95dac69338cc27a963aa9a5dc53e87988a
beaver10 <wkdgkdud1005@gmail.com>
2023-02-02 14:35:49 +0900

    2

---
+++ src/main/java/com/iu/s1/army/Soldier.java
@@ -0,0 +1,48 @@
+package com.iu.s1.army;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Soldier {
+//	private Gun gun;
+	@Autowired
+	@Qualifier("sg")
+	private Gun shotGun;
+	@Autowired
+	@Qualifier("rf")
+	private Gun rifle;
+	
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+//	public Gun getGun() {
+//		return gun;
+//	}
+//@Autowired
+//	public void setGun(Gun gun) {
+//		this.gun = gun;
+//	}
+//
+//	public void useGun() {
+//		this.gun.trigger();
+//	}
+
+}
